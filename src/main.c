829206761b9d1577fb7a5088cbfdf93452266ed7
Bjarki Arge Andreasen <baa@trackunit.com>
2022-12-16 07:56:39 +0100

    updated licence fields

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022 Trackunit Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart.h>
--- src/modem_pipe_uart.c
+++ src/modem_pipe_uart.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022 Trackunit Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <string.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/uart.h>
